c8a0c867096e0ac7fb77100bbf47f688287ac011
이시은 <lsy53430@naver.com>
2024-04-17 14:28:07 +0900

    Init: 디렉터리 생성 및 프론트엔드 프로젝트 생성

---
+++ frontend/kiosk/src/service-worker.js
@@ -0,0 +1,26 @@
+// service-worker.js
+// 임시로 작성한 파일
+
+// Cache names
+const CACHE_NAME = "bbap-kiosk-pwa-cache";
+const urlsToCache = [
+	"/",
+	"/index.html",
+	// Add paths to other static assets you want to cache
+];
+
+// Install event
+self.addEventListener("install", (event) => {
+	event.waitUntil(
+		caches.open(CACHE_NAME).then((cache) => cache.addAll(urlsToCache))
+	);
+});
+
+// Fetch event
+self.addEventListener("fetch", (event) => {
+	event.respondWith(
+		caches
+			.match(event.request)
+			.then((response) => response || fetch(event.request))
+	);
+});
