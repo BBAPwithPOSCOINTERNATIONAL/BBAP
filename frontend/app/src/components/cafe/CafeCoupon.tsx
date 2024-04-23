f2fda4dd9f7befc3eb55f3b5df80c06a18eea499
박영진 <dud_wls_95@naver.com>
2024-04-23 17:59:40 +0900

    Feat: 앱 카페 혼자주문 진행중

--- frontend/app/src/components/cafe/CafeCoupon.tsx
+++ frontend/app/src/components/cafe/CafeCoupon.tsx
@@ -12,25 +12,29 @@ const CafeCoupon: React.FC<CafeCouponStatusProps> = ({
   gaugeBars,
 }) => {
   return (
-    <div className="mt-1 flex flex-col items-center">
-      <p className="mt-1 font-hyemin-bold text-left">
+    <>
+      <p className="mt-1 font-hyemin-bold ml-5 mt-1">
         현재 주문량: {orderCount}개
       </p>
-      <div className="bg-cafe-primary-color border p-3 rounded-lg mt-4 w-11/12 font-hyemin-bold">
-        <div className="mt-1">
-          <div className="flex justify-between">
-            <p>보유 스탬프</p>
-            <p>보유 쿠폰: {couponCount}장</p>
-          </div>
-          <div className="mt-2 text-xs">
-            10개를 모으면 쿠폰으로 사용할 수 있어요
-          </div>
-          <div className="flex justify-center mt-4">
-            <div className="h-4 w-full border rounded-lg flex">{gaugeBars}</div>
+      <div className="mt-1 flex flex-col items-center">
+        <div className="bg-cafe-primary-color border p-3 rounded-lg mt-4 w-11/12 font-hyemin-bold">
+          <div>
+            <div className="flex justify-between">
+              <p>보유 스탬프</p>
+              <p>보유 쿠폰: {couponCount}장</p>
+            </div>
+            <div className="mt-2 text-xs">
+              10개를 모으면 쿠폰으로 사용할 수 있어요
+            </div>
+            <div className="flex justify-center mt-4">
+              <div className="h-4 w-full border rounded-lg flex">
+                {gaugeBars}
+              </div>
+            </div>
           </div>
         </div>
       </div>
-    </div>
+    </>
   );
 };
 
--- frontend/app/src/pages/cafe/CafeMainPage.tsx
+++ frontend/app/src/pages/cafe/CafeMainPage.tsx
@@ -1,27 +1,93 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import NavBar from "../../components/Navbar";
 import CafeTabs from "../../components/cafe/CafeTabs";
 import CafeCoupon from "../../components/cafe/CafeCoupon";
 import menu from "../../assets/cafe-menu.json";
+import MenuSection from "../../components/cafe/MenuSection";
 
-interface MenuItem {
-  name: string;
-  price: number;
-  description: string;
-  images: string;
-  temperature: string[];
-  size: string[];
-  options: { [key: string]: number };
+// interface MenuItem {
+//   name: string;
+//   price: number;
+//   description: string;
+//   images: string;
+//   temperature?: string[];
+//   size?: string[];
+//   options?: { [key: string]: number };
+// }
+
+import { useRef } from "react";
+import MenuButtons from "../../components/cafe/MenuButtons";
+
+function useMoveScroll(offset = 0) {
+  const element = useRef<HTMLDivElement>(null);
+  const onMoveToElement = () => {
+    if (element.current) {
+      const elementPosition =
+        element.current.getBoundingClientRect().top + window.pageYOffset;
+      const offsetPosition = elementPosition - offset;
+      window.scrollTo({ top: offsetPosition, behavior: "smooth" });
+    }
+  };
+  return { element, onMoveToElement };
 }
 
 function CafeMainPage() {
   const [content, setContent] = useState("alone");
   const [selectedCafe, setSelectedCafe] = useState("A카페");
+  const [selectedMenu, setSelectedMenu] = useState("coffee");
+
   const tabs = [
     { key: "alone", label: "혼자주문" },
     { key: "together", label: "같이주문" },
     { key: "history", label: "나의주문" },
   ];
+
+  // const coffeeRef = useRef<HTMLDivElement>(null);
+  // const beverageRef = useRef<HTMLDivElement>(null);
+  // const dessertRef = useRef<HTMLDivElement>(null);
+
+  const handleMenuSelect = (menu: string) => {
+    setSelectedMenu(menu);
+    if (menu === "coffee") scrollToCoffee();
+    else if (menu === "beverage") scrollToBeverage();
+    else if (menu === "dessert") scrollToDessert();
+  };
+
+  useEffect(() => {
+    const handleScroll = () => {
+      const middleScreen = window.innerHeight / 2;
+
+      const coffeePosition = coffeeRef.current?.getBoundingClientRect();
+      const beveragePosition = beverageRef.current?.getBoundingClientRect();
+      const dessertPosition = dessertRef.current?.getBoundingClientRect();
+
+      if (
+        dessertPosition &&
+        dessertPosition.top <= middleScreen &&
+        dessertPosition.bottom >= middleScreen
+      ) {
+        setSelectedMenu("dessert");
+      } else if (
+        beveragePosition &&
+        beveragePosition.top <= middleScreen &&
+        beveragePosition.bottom >= middleScreen
+      ) {
+        setSelectedMenu("beverage");
+      } else if (
+        coffeePosition &&
+        coffeePosition.top <= middleScreen &&
+        coffeePosition.bottom >= middleScreen
+      ) {
+        setSelectedMenu("coffee");
+      }
+    };
+
+    window.addEventListener("scroll", handleScroll);
+
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
 
   const [orderCount, setOrderCount] = useState(10);
   const [couponCount, setCouponCount] = useState(9);
@@ -43,45 +109,69 @@ function CafeMainPage() {
     ></div>
   ));
 
+  const navBarHeight = 50; // NavBar의 높이 추정값
+  const tabsHeight = 50; // CafeTabs의 높이 추정값
+  const couponAndButtonsHeight = 10; // CafeCoupon과 버튼 그룹의 높이 추정값
+
+  const totalOffset = navBarHeight + tabsHeight + couponAndButtonsHeight;
+
+  const { element: coffeeRef, onMoveToElement: scrollToCoffee } = useMoveScroll(
+    totalOffset + 270
+  );
+  const { element: beverageRef, onMoveToElement: scrollToBeverage } =
+    useMoveScroll(totalOffset + 270);
+  const { element: dessertRef, onMoveToElement: scrollToDessert } =
+    useMoveScroll(totalOffset + 270);
+
   return (
     <div>
-      <NavBar />
-      <CafeTabs content={content} setContent={setContent} tabs={tabs} />
-      <div className="mt-8">
+      <div
+        className="sticky top-0 z-30 bg-white"
+        style={{ height: `${navBarHeight}px` }}
+      >
+        <NavBar />
+      </div>
+
+      <div
+        className="sticky top-[65px] z-20 bg-white"
+        style={{ height: `${tabsHeight}px` }}
+      >
+        <CafeTabs content={content} setContent={setContent} tabs={tabs} />
+      </div>
+      <div>
         {content === "alone" && (
           <>
-            <div className="mt-2 flex flex-col items-center">
-              <select
-                id="cafe-select"
-                value={selectedCafe}
-                onChange={(e) => setSelectedCafe(e.target.value)}
-                className="bg-primary-color text-white border rounded-md p-1 w-11/12 font-hyemin-bold text-center"
-              >
-                <option value="A카페">A카페</option>
-                <option value="B카페">B카페</option>
-                <option value="C카페">C카페</option>
-              </select>
+            <div
+              className="sticky top-[110px] z-10 bg-white"
+              style={{ paddingTop: `${couponAndButtonsHeight}px` }}
+            >
+              <div className="mt-2 flex flex-col items-center">
+                <select
+                  id="cafe-select"
+                  value={selectedCafe}
+                  onChange={(e) => setSelectedCafe(e.target.value)}
+                  className="bg-primary-color text-white border rounded-md p-1 w-11/12 font-hyemin-bold text-center"
+                >
+                  <option value="A카페">A카페</option>
+                  <option value="B카페">B카페</option>
+                  <option value="C카페">C카페</option>
+                </select>
+              </div>
+              <CafeCoupon
+                orderCount={orderCount}
+                couponCount={couponCount}
+                gaugeBars={gaugeBars}
+              />
+              <div className="flex mt-1">
+                <MenuButtons
+                  selectedMenu={selectedMenu}
+                  onSelectMenu={handleMenuSelect}
+                />
+              </div>
             </div>
-            <CafeCoupon
-              orderCount={orderCount}
-              couponCount={couponCount}
-              gaugeBars={gaugeBars}
-            />
-            <ul>
-              {menu.coffee.map((item: MenuItem, index: number) => (
-                <li key={index}>
-                  <h2>
-                    {item.name} - {item.price}원
-                  </h2>
-                  <p>{item.description}</p>
-                  <img
-                    src={item.images}
-                    alt={item.name}
-                    style={{ width: "100%" }}
-                  />
-                </li>
-              ))}
-            </ul>
+            <MenuSection ref={coffeeRef} items={menu.coffee} title="커피" />
+            <MenuSection ref={beverageRef} items={menu.beverage} title="음료" />
+            <MenuSection ref={dessertRef} items={menu.dessert} title="디저트" />
           </>
         )}
         {content === "together" && <div>같이 주문하는 페이지입니다.</div>}
