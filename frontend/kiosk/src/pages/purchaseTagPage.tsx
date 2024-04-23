7ac657fcfb675621e2c46dd30de8779c397d287f
이시은 <lsy53430@naver.com>
2024-04-23 18:02:38 +0900

    Feat: 사원증 태그 인식

--- frontend/kiosk/src/pages/purchaseTagPage.tsx
+++ frontend/kiosk/src/pages/purchaseTagPage.tsx
@@ -1,14 +1,60 @@
-import React from "react";
+import React, { useState, useEffect, useRef } from "react";
 import { useNavigate } from "react-router";
 import Header from "../components/header";
 import Button from "../components/button";
 
+// 한영 전환 수정 필요함
+const koreanKeys = "ㅂㅈㄷㄱㅅㅛㅕㅑㅐㅔㅁㄴㅇㄹㅎㅗㅓㅏㅣㅋㅌㅊㅍㅠㅜㅡ";
+const englishKeys =
+	"qwe r t y u i o p a s d f g h j k l ; ' z x c v b n m , . /";
+
 const PurchaseTagPage: React.FC = () => {
 	const navigation = useNavigate();
+	const [tagValue, setTagValue] = useState<string>("");
+	const inputRef = useRef<HTMLInputElement>(null);
+
+	useEffect(() => {
+		if (inputRef.current) {
+			inputRef.current.focus();
+		}
+	});
+
+	const activeEnter = (e: React.KeyboardEvent) => {
+		if (e.key === "Enter") {
+			let result = "";
+			for (let char of tagValue) {
+				const idx = koreanKeys.indexOf(char);
+				if (idx !== -1) {
+					result += englishKeys[idx];
+				} else {
+					result += char;
+				}
+			}
+
+			console.log(result);
+			setTagValue("");
+			// TODO:
+			// 키보드 영문 입력만 되도록 앱 차원에서 막아야 함
+			// 카드번호 담아서 서버에 사원정보 요청 => 에러나면 에러 모달 뜨도록 하기
+		}
+	};
+
 	return (
 		<>
 			<Header text="결제하기" className="" />
 			<div id="body">
+				<input
+					ref={inputRef}
+					type="text"
+					id="tag-value"
+					style={{ imeMode: "inactive" }}
+					// className="absolute top-[-9999px] w-2"
+					value={tagValue}
+					onChange={(e) => setTagValue(e.target.value)}
+					onKeyDown={(e) => {
+						activeEnter(e);
+					}}
+				/>
 				<div className="flex space-x-20 my-48 justify-center">
 					<div
 						className="border border-2 border-primary-color bg-[#E2F1FF] rounded-2xl w-[850px] h-[1100px] px-10"
