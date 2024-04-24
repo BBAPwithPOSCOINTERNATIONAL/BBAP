e5fefa4bb3e15e608e3420fb7f7382f4122ae883
이성완 <cjddktkdwk@naver.com>
2024-04-24 17:46:23 +0900

    Refactor : api url 추가
    -router 이용해서 /api/v1 prefix추가

--- backend/face_fast_api/main.py
+++ backend/face_fast_api/main.py
@@ -1,14 +1,20 @@
-from fastapi import FastAPI, File, UploadFile, Form
+from fastapi import FastAPI, APIRouter, File, UploadFile, Form
 from face import train, predict
 
-app = FastAPI()
+app = FastAPI(swagger_ui=True)
 
-@app.post("/train/")
+# APIRouter를 사용하여 /api/v1/ 아래에 들어갈 경로를 묶음
+router = APIRouter(prefix="/api/v1", tags=["api"])
+
+@router.post("/train",tags=["api"])
 async def train_model(file: UploadFile = File(...), empId: str = Form(...)):
 
     return {"message": await train(file, empId)}
 
-@app.post("/predict/")
+@router.post("/predict",tags=["api"])
 async def predict_image(file: UploadFile = File(...)):
     
     return {"predictions": await predict(file)}
+
+# 앱에 라우터 추가
+app.include_router(router)
