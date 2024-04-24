f0df78bfdc787be6a3bf4d653a3798265b53c300
김다희 <kdahui98@gmail.com>
2024-04-24 17:50:09 +0900

    Update GlobalExceptionHandler.java

--- backend/cafe/src/main/java/com/bbap/cafe/config/GlobalExceptionHandler.java
+++ backend/cafe/src/main/java/com/bbap/cafe/config/GlobalExceptionHandler.java
@@ -6,12 +6,26 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 import com.bbap.cafe.dto.responseDto.ResponseDto;
+import com.bbap.cafe.exception.CafeEntityNotFoundException;
+import com.bbap.cafe.exception.CustomException;
 
 @RestControllerAdvice
 public class GlobalExceptionHandler {
+	//유효성 검사
 	@ExceptionHandler(MethodArgumentNotValidException.class)
 	public ResponseEntity<ResponseDto> validationExceptionHandler(MethodArgumentNotValidException e) {
 		ResponseDto responseBody = new ResponseDto("유효성 실패");
+		return ResponseEntity.status(e.getStatusCode()).body(responseBody);
+	}
+
+    //존재하지 않는 카페 아이디
+	@ExceptionHandler(CafeEntityNotFoundException.class)
+	public ResponseEntity<ResponseDto> FaceNotFoundExceptionHandler(CafeEntityNotFoundException e) {
+		return handleException(e);
+	}
+
+	private ResponseEntity<ResponseDto> handleException(CustomException e) {
+		ResponseDto responseBody = new ResponseDto(e.getMessage());
 		return ResponseEntity.status(e.getStatusCode()).body(responseBody);
 	}
 }
