0ecb8ef3d5f36f0908aa1e50c5564f1ad73370ea
박영진 <dud_wls_95@naver.com>
2024-04-28 20:40:49 +0900

    Fix: 로그인 페이지 수정

--- frontend/app/src/pages/users/LoginPage.tsx
+++ frontend/app/src/pages/users/LoginPage.tsx
@@ -1,11 +1,58 @@
-import React, { useState, FormEvent } from "react";
-import logoimg from "../../assets/logo.png";
+import React, { useState, useCallback, useRef, FormEvent } from "react";
 import { useNavigate } from "react-router-dom";
+// import axios from "axios";
+import logoimg from "../../assets/logo.png";
 
 function LoginPage() {
+  const navigate = useNavigate();
+  // const [loading, setLoading] = useState<boolean>(false);
   const [employeeId, setEmployeeId] = useState<string>("");
   const [password, setPassword] = useState<string>("");
-  const navigate = useNavigate();
+
+  const employeeIdRef = useRef<HTMLInputElement | null>(null);
+  const passwordRef = useRef<HTMLInputElement | null>(null);
+
+  const onChangeId = useCallback((e: React.ChangeEvent<HTMLInputElement>) => {
+    setEmployeeId(e.target.value.trim());
+  }, []);
+
+  const onChangePassword = useCallback(
+    (e: React.ChangeEvent<HTMLInputElement>) => {
+      setPassword(e.target.value.trim());
+    },
+    []
+  );
+
+  //  const onSubmit = useCallback(
+  //    async (event: FormEvent) => {
+  //      event.preventDefault();
+  //      if (loading) {
+  //        return;
+  //      }
+  //      if (!employeeId || !password) {
+  //        alert("사원번호와 비밀번호를 모두 입력해주세요.");
+  //        return;
+  //      }
+  //      setLoading(true);
+  //      try {
+  //        const response = await axios.post(`${Config.API_URL}/login`, {
+  //          employeeId,
+  //          password,
+  //        });
+  //        console.log(response.data);
+  //        alert("로그인 되었습니다.");
+  //        // 세션 또는 로컬 스토리지에 토큰 저장 예제
+  //        sessionStorage.setItem("accessToken", response.data.data.accessToken);
+  //        navigate("/main"); // 로그인 성공 후 이동할 경로
+  //      } catch (error) {
+  //        alert("로그인 실패: " + error.response.data.message);
+  //      } finally {
+  //        setLoading(false);
+  //      }
+  //    },
+  //    [loading, email, password, navigate]
+  //  );
+
   const handleLogin = (event: FormEvent<HTMLFormElement>) => {
     event.preventDefault();
     console.log("Login Attempted with:", employeeId, password);
@@ -23,7 +70,9 @@ function LoginPage() {
               type="text"
               id="employeeId"
               value={employeeId}
-              onChange={(e) => setEmployeeId(e.target.value)}
+              onChange={onChangeId}
+              ref={employeeIdRef}
+              // required
               placeholder="사원번호"
               className="font-hyemin-bold shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
             />
@@ -33,7 +82,9 @@ function LoginPage() {
               type="password"
               id="password"
               value={password}
-              onChange={(e) => setPassword(e.target.value)}
+              onChange={onChangePassword}
+              ref={passwordRef}
+              // required
               placeholder="비밀번호"
               className="font-hyemin-bold shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline"
             />
