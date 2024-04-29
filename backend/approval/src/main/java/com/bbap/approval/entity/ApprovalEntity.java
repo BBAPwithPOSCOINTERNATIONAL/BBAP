619d737dc9e7998a582304f0b4d794bbdc2517d6
강성은 <1ksy3629@naver.com>
2024-04-29 17:09:47 +0900

    Feat : 결재 init

---
+++ backend/approval/src/main/java/com/bbap/approval/entity/ApprovalEntity.java
@@ -0,0 +1,17 @@
+package com.bbap.approval.entity;
+
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class ApprovalEntity {
+
+    private Long approvalId;
+
+    private Integer empId;
+
+    private Boolean approvalStatus;
+
+}
