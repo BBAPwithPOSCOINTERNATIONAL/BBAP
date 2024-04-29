ec7e1c0a7043cef5ab2bd4896a79588a14372132
이시은 <lsy53430@naver.com>
2024-04-29 16:04:22 +0900

    Feat: 키오스크-얼굴인식 코드 이식

--- frontend/kiosk/src/pages/paymentFacePage.tsx
+++ frontend/kiosk/src/pages/paymentFacePage.tsx
@@ -1,13 +1,106 @@
-import React from "react";
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import React, { useRef, useEffect, useState, useCallback } from "react";
 import { useNavigate } from "react-router";
 import Header from "../components/header";
 import Button from "../components/button";
-import Webcam from "react-webcam";
+import * as faceapi from "face-api.js";
+import { payInfoByFace } from "../api/paymentApi";
 
 const PaymentFacePage: React.FC = () => {
+	const videoRef = useRef<HTMLVideoElement>(null);
+	const canvasRef = useRef<HTMLDivElement>(null);
+	const [warningMsg, setWarningMsg] = useState<string>("");
+
 	const navigate = useNavigate();
-	// eslint-disable-next-line @typescript-eslint/no-explicit-any
-	const webcamRef = React.useRef<any>(null);
+
+	useEffect(() => {
+		const loadModels = async () => {
+			const MODEL_URL = "/models";
+			await (faceapi.nets.tinyFaceDetector as any).loadFromUri(MODEL_URL);
+			await (faceapi.nets.faceLandmark68Net as any).loadFromUri(MODEL_URL);
+			await (faceapi.nets.faceRecognitionNet as any).loadFromUri(MODEL_URL);
+			console.log("Models loaded");
+		};
+		const startVideo = () => {
+			navigator.mediaDevices
+				.getUserMedia({ video: {} })
+				.then((stream) => {
+					if (videoRef.current) {
+						videoRef.current.srcObject = stream;
+					}
+				})
+				.catch((err) => {
+					console.error("Failed to get video stream", err);
+				});
+		};
+
+		loadModels().then(startVideo);
+	}, []);
+
+	const detection = useCallback(async () => {
+		if (!videoRef.current || videoRef.current.paused || videoRef.current.ended)
+			return;
+		const detections = await faceapi
+			.detectAllFaces(videoRef.current, new faceapi.TinyFaceDetectorOptions())
+			.withFaceLandmarks()
+			.withFaceDescriptors();
+		if (detections.length > 0) {
+			captureImage();
+			return;
+		}
+		requestAnimationFrame(detection);
+	}, []);
+
+	useEffect(() => {
+		if (videoRef.current) {
+			videoRef.current.addEventListener("play", detection);
+		}
+
+		return () => {
+			if (videoRef.current) {
+				videoRef.current.removeEventListener("play", detection);
+			}
+		};
+	}, [detection]);
+
+	const retryDetection = () => {
+		setWarningMsg("");
+		if (videoRef.current) {
+			videoRef.current.play().then(() => {
+				detection(); // Restart the detection process
+			});
+		}
+	};
+
+	const captureImage = () => {
+		const canvas = document.createElement("canvas");
+		const context = canvas.getContext("2d");
+		if (context && videoRef.current) {
+			canvas.width = videoRef.current.videoWidth;
+			canvas.height = videoRef.current.videoHeight;
+			context.drawImage(videoRef.current, 0, 0, canvas.width, canvas.height);
+
+			// Convert data URL to file
+			canvas.toBlob((blob) => {
+				if (blob) {
+					const file = new File([blob], "captured.png", { type: "image/png" });
+					sendImageDataToServer(file);
+				} else {
+					console.error("Failed to convert canvas data to Blob");
+				}
+			}, "image/png");
+		}
+	};
+
+	const sendImageDataToServer = async (file: File) => {
+		try {
+			const response = await payInfoByFace(file);
+			navigate("/payment-final", { state: response.data });
+		} catch (err) {
+			setWarningMsg("등록되지 않은 사원입니다");
+		}
+	};
+
 	return (
 		<>
 			<Header text="결제하기" className="" />
@@ -27,21 +120,36 @@ const PaymentFacePage: React.FC = () => {
 					<div className="absolute top-0 left-0 w-full h-full flex justify-center items-center z-10">
 						<img src="assets/images/guideLine.png" alt="" className="w-2/3" />
 					</div>
-					<Webcam
-						audio={false}
+					<video
+						ref={videoRef}
+						autoPlay
+						muted
 						height="100%"
 						width="100%"
-						ref={webcamRef}
-						screenshotFormat="image/jpeg"
 						style={{
 							position: "absolute",
 							left: "50%",
 							top: "50%",
-							transform: "translate(-50%, -50%)",
+							transform: "translate(-50%, -50%) scaleX(-1)",
 						}}
 					/>
+					<div ref={canvasRef} />
 				</div>
-				{/* TODO: 얼굴인식 되면 체크표시, 문제가 있어서 안되면 X 표시 뜨도록 해야함 */}
+				{warningMsg && (
+					<div>
+						<p className="text-xl text-red-500 font-bold text-center mt-10 mb-5">
+							{warningMsg}
+						</p>
+						<div
+							className="bg-red-500 text-white rounded-full text-lg w-1/4 py-5 mx-auto text-center"
+							onClick={() => {
+								retryDetection();
+							}}
+						>
+							재시도
+						</div>
+					</div>
+				)}
 			</div>
 			<div className="w-full absolute bottom-[150px] text-center">
 				<Button
