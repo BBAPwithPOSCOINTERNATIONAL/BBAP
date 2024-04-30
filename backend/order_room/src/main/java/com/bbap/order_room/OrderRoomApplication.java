c9160edd0da7ad3d12ba04359d5d94448cf96e95
김다희 <kdahui98@gmail.com>
2024-04-30 11:34:15 +0900

    init: 주문방 시작

---
+++ backend/order_room/src/main/java/com/bbap/order_room/OrderRoomApplication.java
@@ -0,0 +1,13 @@
+package com.bbap.order_room;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrderRoomApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OrderRoomApplication.class, args);
+	}
+
+}
---
+++ backend/order_room/src/test/java/com/bbap/order_room/OrderRoomApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bbap.order_room;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrderRoomApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
