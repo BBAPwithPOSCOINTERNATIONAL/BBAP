eebdead521ddd1d2b09830a98da454f021542042
이성완 <cjddktkdwk@naver.com>
2024-05-03 23:21:18 +0900

    Refactor : feign url
    - properties에서 url 가져오게 변경

--- backend/restaurant/src/main/java/com/bbap/restaurant/feign/HrServiceFeignClient.java
+++ backend/restaurant/src/main/java/com/bbap/restaurant/feign/HrServiceFeignClient.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.RequestHeader;
 import com.bbap.restaurant.dto.response.DataResponseDto;
 import com.bbap.restaurant.dto.response.EmployeeDto;
 
-@FeignClient(name = "hr", url = "http://localhost:8080/api/v1/hr")
+@FeignClient(name = "hr", url = "${feign_hr}")
 public interface HrServiceFeignClient {
 	@GetMapping("/auth/user-info")
 	ResponseEntity<DataResponseDto<EmployeeDto>> getUserInfo(@RequestHeader(value = "X-Employee-Id") int empId);
