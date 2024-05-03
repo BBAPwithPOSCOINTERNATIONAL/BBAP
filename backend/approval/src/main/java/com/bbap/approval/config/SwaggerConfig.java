34098c332f16a45718d11341346bc2f3885d805d
이성완 <cjddktkdwk@naver.com>
2024-05-03 16:25:47 +0900

    Feat : swagger
    - 스웨거 설정 추가

---
+++ backend/approval/src/main/java/com/bbap/approval/config/SwaggerConfig.java
@@ -0,0 +1,20 @@
+package com.bbap.approval.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+
+@Configuration
+public class SwaggerConfig {
+
+	@Bean
+	public OpenAPI openAPI() {
+		return new OpenAPI()
+			.info(new Info()
+				.title("BBAP_APPROVAL")
+				.description("대규모 프로젝트.")
+				.version("1.0.0"));
+	}
+}
