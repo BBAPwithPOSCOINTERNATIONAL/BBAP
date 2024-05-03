9a4e93d294bf4a954760a75850c3feb07548e206
이성완 <cjddktkdwk@naver.com>
2024-05-03 23:21:01 +0900

    Refactor : feign url
    - properties에서 url 가져오게 변경

--- backend/payment/src/main/java/com/bbap/payment/feign/HrServiceFeignClient.java
+++ backend/payment/src/main/java/com/bbap/payment/feign/HrServiceFeignClient.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import com.bbap.payment.dto.response.CheckEmpResponseData;
 import com.bbap.payment.dto.response.DataResponseDto;
 
-@FeignClient(name = "hr", url = "http://localhost:8080/api/v1/hr")
+@FeignClient(name = "hr", url = "${feign_hr}")
 public interface HrServiceFeignClient {
 	@GetMapping("/employees/card/{empCard}")
 	ResponseEntity<DataResponseDto<CheckEmpResponseData>> checkCard(@PathVariable String empCard);
