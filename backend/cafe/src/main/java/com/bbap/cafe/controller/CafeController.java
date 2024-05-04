44362e52152c9a29afc86194c4270db807a5fab5
강성은 <1ksy3629@naver.com>
2024-05-04 15:48:59 +0900

    Feat : 카페 CORS 어노테이션 추가

--- backend/cafe/src/main/java/com/bbap/cafe/controller/CafeController.java
+++ backend/cafe/src/main/java/com/bbap/cafe/controller/CafeController.java
@@ -1,10 +1,7 @@
 package com.bbap.cafe.controller;
 
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import com.bbap.cafe.dto.response.CafeInfoOrderListDto;
 import com.bbap.cafe.dto.response.CafeListDto;
@@ -21,6 +18,7 @@ import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 
+@CrossOrigin(origins = "*", allowedHeaders = "*")
 @RestController
 @RequiredArgsConstructor
 @RequestMapping(value = "/api/v1/cafes", produces = "application/json; charset=UTF8")
