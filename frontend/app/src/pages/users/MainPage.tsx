4ea5fbe584fde5e777e285f6e8f296d84c81d8dd
박영진 <dud_wls_95@naver.com>
2024-05-04 21:21:27 +0900

    Feat: 메인페이지 api 연결

--- frontend/app/src/pages/users/MainPage.tsx
+++ frontend/app/src/pages/users/MainPage.tsx
@@ -1,14 +1,33 @@
 import NavBar from "../../components/Navbar";
 import BottomTabBar from "../../components/BottomTabBar";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { useUserStore } from "../../store/userStore";
+import { getMonthlyPayments, PaymentData } from "../../api/paymentsAPI";
 
 function MainPage() {
   // const [error, setError] = useState<string | null>(null);
   const userInfo = useUserStore((state) => state);
+  const [paymentData, setPaymentData] = useState<PaymentData | null>(null);
+  const today = new Date();
+
+  const getCurrentDate = () => {
+    const year = today.getFullYear();
+    const month = (today.getMonth() + 1).toString().padStart(2, "0");
+    return `${year}-${month}`;
+  };
 
   useEffect(() => {
-    console.log(userInfo);
+    const fetchPayments = async () => {
+      try {
+        const response = await getMonthlyPayments(getCurrentDate());
+        // console.log(response.data);
+        setPaymentData(response.data);
+      } catch (error) {
+        console.error("Failed to fetch payment data:", error);
+      }
+    };
+
+    fetchPayments();
   }, []);
 
   return (
@@ -18,7 +37,7 @@ function MainPage() {
         <div className="text-center mt-8">
           {userInfo && userInfo.empName ? (
             <h1 className="text-4xl font-hyemin-bold text-white">
-              {userInfo.empName} 님의 O월
+              {userInfo.empName} 님의 {today.getMonth() + 1}월
             </h1>
           ) : (
             <h1 className="text-4xl font-hyemin-bold text-white">
@@ -26,19 +45,27 @@ function MainPage() {
             </h1>
           )}
           <p className="text-4xl mt-4 font-hyemin-bold text-white">BBAP 로그</p>
-          {/* 추가 정보를 여기에 표시할 수 있습니다. 예를 들어: */}
           <div className="mt-8">
-            {userInfo && (
+            {paymentData && (
               <>
-                <div className="text-xl font-hyemin-bold text-white">
-                  부서: {userInfo.department?.departmentName}
+                <div className="text-2xl font-hyemin-bold text-white">
+                  총 결제 금액
                 </div>
-                <div className="text-xl font-hyemin-bold text-white">
-                  포지션: {userInfo.position?.positionName}
+                <p className="text-2xl font-hyemin-bold text-primary-color text-white">
+                  {paymentData.totalPaymentAmountSum.toLocaleString()} 원
+                </p>
+                <div className="text-2xl font-hyemin-bold mt-4 text-white">
+                  총 지원금
                 </div>
-                <div className="text-xl font-hyemin-bold text-white">
-                  사무실: {userInfo.workplace?.workplaceName}
+                <p className="text-2xl font-hyemin-bold text-white">
+                  {paymentData.useSubsidySum.toLocaleString()} 원
+                </p>
+                <div className="text-2xl font-hyemin-bold mt-4 text-white">
+                  본인부담금
                 </div>
+                <p className="text-2xl font-hyemin-bold text-white">
+                  {paymentData.selfPaymentSum.toLocaleString()} 원
+                </p>
               </>
             )}
           </div>
