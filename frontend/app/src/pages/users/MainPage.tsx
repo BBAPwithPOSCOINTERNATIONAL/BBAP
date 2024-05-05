64f61a16e93dfa074daa2c4794d9fdc8880e1033
조혜원 <a01051910573@gmail.com>
2024-05-05 20:06:30 +0900

    Fix: build 오류 수정

--- frontend/app/src/pages/users/MainPage.tsx
+++ frontend/app/src/pages/users/MainPage.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import  { useEffect, useState } from "react";
 import NavBar from "../../components/Navbar";
 import BottomTabBar from "../../components/BottomTabBar";
 import { useUserStore } from "../../store/userStore";
