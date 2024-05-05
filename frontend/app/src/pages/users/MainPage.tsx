4cd6c2332771e7a1a0dd66e85d25b4089e6e11ae
조혜원 <a01051910573@gmail.com>
2024-05-05 14:18:32 +0900

    Design: mainpage:

--- frontend/app/src/pages/users/MainPage.tsx
+++ frontend/app/src/pages/users/MainPage.tsx
@@ -1,13 +1,37 @@
+import React, { useEffect, useState } from "react";
 import NavBar from "../../components/Navbar";
 import BottomTabBar from "../../components/BottomTabBar";
-import { useEffect, useState } from "react";
 import { useUserStore } from "../../store/userStore";
 import { getMonthlyPayments, PaymentData } from "../../api/paymentsAPI";
+// <a href="https://www.flaticon.com/kr/free-icons/-" title="신용 카드 결제 아이콘">신용 카드 결제 아이콘 제작자: SBTS2018 - Flaticon</a>
+import totalpayment from '/assets/images/main/totalpayment.png';
+// <a href="https://www.flaticon.com/kr/free-icons/" title="돈 아이콘">돈 아이콘 제작자: monkik - Flaticon</a>
+import totalsubsidy from '/assets/images/main/totalsubsidy.png';
+import yours from '/assets/images/main/yours.png';
+
 
 function MainPage() {
-  // const [error, setError] = useState<string | null>(null);
   const userInfo = useUserStore((state) => state);
   const [paymentData, setPaymentData] = useState<PaymentData | null>(null);
+  const [carouselIndex, setCarouselIndex] = useState(0);
+  const carouselItems = paymentData ? [
+    {
+      label: "총 결제 금액",
+      picture:totalpayment,
+      value: paymentData.totalPaymentAmountSum.toLocaleString() + ' 원'
+    },
+    {
+      label: "총 지원금",
+      picture: totalsubsidy,
+      value: paymentData.useSubsidySum.toLocaleString() + ' 원'
+    },
+    {
+      label: "본인부담금",
+      picture: yours,
+      value: paymentData.selfPaymentSum.toLocaleString() + ' 원'
+    }
+  ] : [];
+
   const today = new Date();
 
   const getCurrentDate = () => {
@@ -20,7 +44,6 @@ function MainPage() {
     const fetchPayments = async () => {
       try {
         const response = await getMonthlyPayments(getCurrentDate());
-        // console.log(response.data);
         setPaymentData(response.data);
       } catch (error) {
         console.error("Failed to fetch payment data:", error);
@@ -29,6 +52,14 @@ function MainPage() {
 
     fetchPayments();
   }, []);
+
+  useEffect(() => {
+    const timer = setInterval(() => {
+      setCarouselIndex((prevIndex) => (prevIndex + 1) % carouselItems.length);
+    }, 3000); // Rotate every 3 seconds
+
+    return () => clearInterval(timer);
+  }, [carouselItems.length]);
 
   return (
     <div className="flex flex-col min-h-screen bg-bg-color overflow-hidden">
@@ -44,30 +75,22 @@ function MainPage() {
               Loading or error...
             </h1>
           )}
-          <p className="text-4xl mt-4 font-hyemin-bold text-white">BBAP 로그</p>
-          <div className="mt-8">
-            {paymentData && (
+          <p className="text-4xl mt-4 font-hyemin-bold text-white">BBAP 기록</p>
+          <div className="mt-8 w-68 bg-amber-50 rounded-lg z-0" style={{height: '30rem'}}>
+            <div className="z-10 pt-14">
+
+            {paymentData && carouselItems.length > 0 && (
               <>
-                <div className="text-2xl font-hyemin-bold text-white">
-                  총 결제 금액
+                <div className="text-5xl font-hyemin-bold mb-8">
+                  {carouselItems[carouselIndex].label}
                 </div>
-                <p className="text-2xl font-hyemin-bold text-primary-color text-white">
-                  {paymentData.totalPaymentAmountSum.toLocaleString()} 원
-                </p>
-                <div className="text-2xl font-hyemin-bold mt-4 text-white">
-                  총 지원금
-                </div>
-                <p className="text-2xl font-hyemin-bold text-white">
-                  {paymentData.useSubsidySum.toLocaleString()} 원
-                </p>
-                <div className="text-2xl font-hyemin-bold mt-4 text-white">
-                  본인부담금
-                </div>
-                <p className="text-2xl font-hyemin-bold text-white">
-                  {paymentData.selfPaymentSum.toLocaleString()} 원
+                <img src={carouselItems[carouselIndex].picture} alt="Carousel Image"  className="h-40 mb-8 mx-auto" />
+                 <p className="text-4xl font-hyemin-bold text-primary-color">
+                  {carouselItems[carouselIndex].value}
                 </p>
               </>
             )}
+            </div>
           </div>
         </div>
       </div>
