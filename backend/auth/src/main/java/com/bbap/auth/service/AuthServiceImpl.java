6d18bf7773a09a22e360ffab5d80c25e7d51e271
강성은 <1ksy3629@naver.com>
2024-05-06 18:12:35 +0900

    Delete : 필요없는 주석 제거

--- backend/auth/src/main/java/com/bbap/auth/service/AuthServiceImpl.java
+++ backend/auth/src/main/java/com/bbap/auth/service/AuthServiceImpl.java
@@ -17,42 +17,6 @@ import org.springframework.stereotype.Service;
 @AllArgsConstructor
 public class AuthServiceImpl implements AuthService{
 
-//    private final JwtProvider jwtProvider;
-
-//    @Override
-//    public boolean isValid(String token) {
-//
-//        String parsedToken = jwtProvider.parseBearerToken(token);
-//        System.out.println(parsedToken);
-//
-//        if (parsedToken == null) {
-//            log.warn("입력받은 토큰이 null : {}", token);
-//            return false;
-//        }
-//
-//
-//        boolean isValid = jwtProvider.validateToken(parsedToken) != null;
-//        log.info("토큰 검증 결과 {}: {}", token, isValid);
-//
-//        return isValid;
-//    }
-//
-//    @Override
-//    public String getUserIdFromToken(String token) {
-//        String parsedToken = jwtProvider.parseBearerToken(token);
-//
-//        Jws<Claims> validatedToken = jwtProvider.validateToken(parsedToken);
-//        if (validatedToken != null) {
-//
-//            Claims claims = validatedToken.getPayload();
-//            Integer employeeId = claims.get("employeeId", Integer.class);
-//
-//            log.info("추출된 사원 아이디 : {}", employeeId); // Logging info about extracted employeeId
-//            return String.valueOf(employeeId);
-//        } else {
-//            return null;
-//        }
-//    }
 
     @Override
     public ResponseEntity<?> getUserId() {
