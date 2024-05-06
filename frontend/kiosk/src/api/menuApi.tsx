94a4c56bbd63d862096d10059f752f3e2240f9ac
박영진 <dud_wls_95@naver.com>
2024-05-07 08:58:08 +0900

    Chore : merge request

--- frontend/kiosk/src/api/menuApi.tsx
+++ frontend/kiosk/src/api/menuApi.tsx
@@ -3,27 +3,27 @@ import apiClient from "./apiClient";
 import { Menu } from "../types";
 
 interface ApiResponse {
-	message: string;
-	data: {
-		menuListCoffee: Menu[];
-		menuListBeverage: Menu[];
-		menuListDessert: Menu[];
-		menuListPopular: Menu[];
-	};
+  message: string;
+  data: {
+    menuListCoffee: Menu[];
+    menuListBeverage: Menu[];
+    menuListDessert: Menu[];
+    menuListPopular: Menu[];
+  };
 }
 
 export async function fetchMenuData(): Promise<ApiResponse> {
-	try {
-		const CAFE_ID = "66276af2412ced9137ecabe9"; // 카페ID 고정
-		const { data } = await apiClient.get<ApiResponse>(
-			`/api/v1/cafes/kiosk/menus/${CAFE_ID}`
-		);
-		return data;
-	} catch (error) {
-		if (axios.isAxiosError(error) && error.response) {
-			throw new Error(error.response.data.message || "Failed to data");
-		} else {
-			throw new Error("Failed to data");
-		}
-	}
+  try {
+    const CAFE_ID = "66276af2412ced9137ecabe9"; // 카페ID 고정
+    const { data } = await apiClient.get<ApiResponse>(
+      `cafes/kiosk/menus/${CAFE_ID}`
+    );
+    return data;
+  } catch (error) {
+    if (axios.isAxiosError(error) && error.response) {
+      throw new Error(error.response.data.message || "Failed to data");
+    } else {
+      throw new Error("Failed to data");
+    }
+  }
 }
--- frontend/kiosk/src/api/paymentApi.tsx
+++ frontend/kiosk/src/api/paymentApi.tsx
@@ -2,147 +2,147 @@ import axios from "axios";
 import apiClient from "./apiClient";
 
 interface PayInfoApiResponse {
-	message: string;
-	data: {
-		empId: number;
-		empName: string;
-		stampCnt: number;
-		availableSubsidy: number;
-	};
+  message: string;
+  data: {
+    empId: number;
+    empName: string;
+    stampCnt: number;
+    availableSubsidy: number;
+  };
 }
 
 interface PayApiResponse {
-	message: string;
-	data: {
-		orderNum: number;
-	};
+  message: string;
+  data: {
+    orderNum: number;
+  };
 }
 
 interface AuthReqData {
-	empNo: string;
-	password: string;
+  empNo: string;
+  password: string;
 }
 
 interface Menu {
-	menuId: string;
-	cnt: number;
-	options: Option[];
+  menuId: string;
+  cnt: number;
+  options: Option[];
 }
 
 interface Option {
-	optionName: string;
-	type: string;
-	required: boolean;
-	choiceOptions: {
-		choiceName: string;
-		price: number;
-	}[];
+  optionName: string;
+  type: string;
+  required: boolean;
+  choiceOptions: {
+    choiceName: string;
+    price: number;
+  }[];
 }
 
 interface PaymentReqData {
-	empId: number;
-	usedSubsidy: number;
-	menuList: Menu[];
+  empId: number;
+  usedSubsidy: number;
+  menuList: Menu[];
 }
 
 const CAFE_ID = "66276af2412ced9137ecabe9"; // 카페ID 고정
 
 // 미완성
 export async function payInfoByFace(file: File): Promise<PayInfoApiResponse> {
-	const formData = new FormData();
-	formData.append("faceImage", file);
-	formData.append("cafeId", CAFE_ID);
+  const formData = new FormData();
+  formData.append("faceImage", file);
+  formData.append("cafeId", CAFE_ID);
 
-	try {
-		const { data } = await apiClient.post<PayInfoApiResponse>(
-			`/api/v1/order/payInfo/face`,
-			formData,
-			{
-				headers: {
-					"Content-Type": "multipart/form-data",
-				},
-			}
-		);
-		return data;
-	} catch (error) {
-		if (axios.isAxiosError(error) && error.response) {
-			throw new Error(error.response.data.message || "Failed to data");
-		} else {
-			throw new Error("Failed to data");
-		}
-	}
+  try {
+    const { data } = await apiClient.post<PayInfoApiResponse>(
+      `/api/v1/order/payInfo/face`,
+      formData,
+      {
+        headers: {
+          "Content-Type": "multipart/form-data",
+        },
+      }
+    );
+    return data;
+  } catch (error) {
+    if (axios.isAxiosError(error) && error.response) {
+      throw new Error(error.response.data.message || "Failed to data");
+    } else {
+      throw new Error("Failed to data");
+    }
+  }
 }
 
 export async function payInfoByCard(
-	cardId: string
+  cardId: string
 ): Promise<PayInfoApiResponse> {
-	try {
-		const { data } = await apiClient.post<PayInfoApiResponse>(
-			`/api/v1/order/payInfo/card`,
-			{ cardId: cardId, cafeId: CAFE_ID }
-		);
-		return data;
-	} catch (error) {
-		if (axios.isAxiosError(error) && error.response) {
-			throw new Error(error.response.data.message || "Failed to data");
-		} else {
-			throw new Error("Failed to data");
-		}
-	}
+  try {
+    const { data } = await apiClient.post<PayInfoApiResponse>(
+      `order/payInfo/card`,
+      { cardId: cardId, cafeId: CAFE_ID }
+    );
+    return data;
+  } catch (error) {
+    if (axios.isAxiosError(error) && error.response) {
+      throw new Error(error.response.data.message || "Failed to data");
+    } else {
+      throw new Error("Failed to data");
+    }
+  }
 }
 
 export async function payInfoByLogin(
-	payload: AuthReqData
+  payload: AuthReqData
 ): Promise<PayInfoApiResponse> {
-	try {
-		const { data } = await apiClient.post<PayInfoApiResponse>(
-			`/api/v1/order/payInfo/auth`,
-			{ ...payload, cafeId: CAFE_ID }
-		);
-		return data;
-	} catch (error) {
-		if (axios.isAxiosError(error) && error.response) {
-			throw new Error(error.response.data.message || "Failed to data");
-		} else {
-			throw new Error("Failed to data");
-		}
-	}
+  try {
+    const { data } = await apiClient.post<PayInfoApiResponse>(
+      `order/payInfo/auth`,
+      { ...payload, cafeId: CAFE_ID }
+    );
+    return data;
+  } catch (error) {
+    if (axios.isAxiosError(error) && error.response) {
+      throw new Error(error.response.data.message || "Failed to data");
+    } else {
+      throw new Error("Failed to data");
+    }
+  }
 }
 
 export async function paymentReq(
-	payload: PaymentReqData
+  payload: PaymentReqData
 ): Promise<PayApiResponse> {
-	try {
-		const { data } = await apiClient.post<PayApiResponse>(
-			`/api/v1/orders/pay/kiosk`,
-			{ ...payload, cafeId: CAFE_ID }
-		);
-		return data;
-	} catch (error) {
-		if (axios.isAxiosError(error) && error.response) {
-			throw new Error(error.response.data.message || "Failed to data");
-		} else {
-			throw new Error("Failed to data");
-		}
-	}
+  try {
+    const { data } = await apiClient.post<PayApiResponse>(`orders/pay/kiosk`, {
+      ...payload,
+      cafeId: CAFE_ID,
+    });
+    return data;
+  } catch (error) {
+    if (axios.isAxiosError(error) && error.response) {
+      throw new Error(error.response.data.message || "Failed to data");
+    } else {
+      throw new Error("Failed to data");
+    }
+  }
 }
 
 // TODO: 식당 카드태깅 결제 요청 -> 응답 형식도 수정 필요
 export async function paymentRestaurantReq(
-	cardId: string
+  cardId: string
 ): Promise<PayApiResponse> {
-	const MENU_ID = "메뉴아이디"; // 메뉴아이디 고정
-	try {
-		const { data } = await apiClient.post<PayApiResponse>(`/api/v1/`, {
-			menuId: MENU_ID,
-			cardId,
-		});
-		return data;
-	} catch (error) {
-		if (axios.isAxiosError(error) && error.response) {
-			throw new Error(error.response.data.message || "Failed to data");
-		} else {
-			throw new Error("Failed to data");
-		}
-	}
+  const MENU_ID = "메뉴아이디"; // 메뉴아이디 고정
+  try {
+    const { data } = await apiClient.post<PayApiResponse>(`/api/v1/`, {
+      menuId: MENU_ID,
+      cardId,
+    });
+    return data;
+  } catch (error) {
+    if (axios.isAxiosError(error) && error.response) {
+      throw new Error(error.response.data.message || "Failed to data");
+    } else {
+      throw new Error("Failed to data");
+    }
+  }
 }
