32aa3f52850576cb6b970857aa20cff860dcdb9a
강성은 <1ksy3629@naver.com>
2024-05-09 14:19:41 +0900

    Feat : 세션 식별자 session Id로 수정

--- backend/order_room/src/main/java/com/bbap/order_room/config/WebSocketConfig.java
+++ backend/order_room/src/main/java/com/bbap/order_room/config/WebSocketConfig.java
@@ -1,7 +1,14 @@
 package com.bbap.order_room.config;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.messaging.Message;
+import org.springframework.messaging.MessageChannel;
+import org.springframework.messaging.simp.config.ChannelRegistration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import org.springframework.messaging.simp.stomp.StompCommand;
+import org.springframework.messaging.simp.stomp.StompHeaderAccessor;
+import org.springframework.messaging.support.ChannelInterceptor;
+import org.springframework.messaging.support.MessageHeaderAccessor;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
 import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
@@ -19,10 +26,24 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
 	public void registerStompEndpoints(StompEndpointRegistry registry) {
 		registry.addEndpoint("/websocket").setAllowedOriginPatterns("https://ssafybbap.com", "http://localhost:3000").withSockJS();
 	}
-	// @Override
-	// public void registerStompEndpoints(StompEndpointRegistry registry) {
-	// 	registry.addEndpoint("/websocket").setAllowedOrigins("*").addInterceptors(
-	// 		new UserIdHandshakeInterceptor()
-	// 	);
-	// }
+
+	@Override
+	public void configureClientInboundChannel(ChannelRegistration registration) {
+		registration.interceptors(new ChannelInterceptor() {
+			@Override
+			public Message<?> preSend(Message<?> message, MessageChannel channel) {
+				StompHeaderAccessor accessor = MessageHeaderAccessor.getAccessor(message, StompHeaderAccessor.class);
+				if (StompCommand.CONNECT.equals(accessor.getCommand())) {
+					accessor.setUser(accessor::getSessionId);
+				}
+				return message;
+			}
+		});
+	}
+//	 @Override
+//	 public void registerStompEndpoints(StompEndpointRegistry registry) {
+//	 	registry.addEndpoint("/websocket").setAllowedOrigins("*").addInterceptors(
+//	 		new UserIdHandshakeInterceptor()
+//	 	);
+//	 }
 }
