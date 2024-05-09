a081a3047892cbe099094ba6925ef6b8b41bb78e
박영진 <dud_wls_95@naver.com>
2024-05-09 11:12:28 +0900

    Chore: merge

--- frontend/kiosk/src/components/button.tsx
+++ frontend/kiosk/src/components/button.tsx
@@ -1,16 +1,16 @@
 import React from "react";
 interface ButtonProps {
-	onClick?: () => void;
-	text: string | React.ReactNode;
-	className: string;
+  onClick?: () => void;
+  text: string | React.ReactNode;
+  className: string;
 }
 
 const Button: React.FC<ButtonProps> = ({ onClick, text, className }) => {
-	return (
-		<button className={`${className} rounded-xl`} onClick={onClick}>
-			{text}
-		</button>
-	);
+  return (
+    <button className={`${className} rounded-xl`} onClick={onClick}>
+      {text}
+    </button>
+  );
 };
 
 export default Button;
