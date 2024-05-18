a2ab89d83e6ce64600cae73bc58719da94a49943
김다희 <kdahui98@gmail.com>
2024-05-18 18:47:13 +0900

    Feat : Gson localDateTime 처리

--- backend/order_room/src/main/java/com/bbap/order_room/config/RedisSubscriber.java
+++ backend/order_room/src/main/java/com/bbap/order_room/config/RedisSubscriber.java
@@ -1,5 +1,7 @@
 package com.bbap.order_room.config;
 
+import java.time.LocalDateTime;
+
 import org.springframework.data.redis.connection.Message;
 import org.springframework.data.redis.connection.MessageListener;
 import org.springframework.messaging.simp.SimpMessagingTemplate;
@@ -8,6 +10,7 @@ import org.springframework.stereotype.Service;
 import com.bbap.order_room.entity.redis.Room;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -18,13 +21,15 @@ import lombok.extern.slf4j.Slf4j;
 public class RedisSubscriber implements MessageListener {
 
     private final SimpMessagingTemplate messagingTemplate;
-
+    private final Gson gson = new GsonBuilder()
+        .registerTypeAdapter(LocalDateTime.class, new LocalDateTimeAdapter())
+        .create();
     @Override
     public void onMessage(Message message, byte[] pattern) {
         try {
             String messageBody = new String(message.getBody());
             log.info("Received message: {}", messageBody);
-            Room room = new Gson().fromJson(messageBody, Room.class);
+            Room room = gson.fromJson(messageBody, Room.class);
             messagingTemplate.convertAndSend("/topic/room/" + room.getRoomId(), room);
             log.info("방 {}의 업데이트 정보가 STOMP 구독자들에게 전송되었습니다.", room.getRoomId());
         } catch (Exception e) {
--- backend/order_room/src/main/java/com/bbap/order_room/service/StompToKafkaService.java
+++ backend/order_room/src/main/java/com/bbap/order_room/service/StompToKafkaService.java
@@ -1,5 +1,8 @@
 package com.bbap.order_room.service;
 
+import java.time.LocalDateTime;
+
+import com.bbap.order_room.config.LocalDateTimeAdapter;
 import com.bbap.order_room.dto.kafkamessage.AddOrderItemMessage;
 import com.bbap.order_room.dto.kafkamessage.ConnectRoomMessage;
 import com.bbap.order_room.dto.kafkamessage.DeleteOrderItemMessage;
@@ -11,6 +14,7 @@ import com.bbap.order_room.dto.requestDto.AddOrderItemRequestDto;
 import com.bbap.order_room.dto.requestDto.OrderRequestDto;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -23,12 +27,15 @@ import org.springframework.stereotype.Service;
 public class StompToKafkaService {
 
     private final KafkaTemplate<String, String> kafkaTemplate;
+    private final Gson gson = new GsonBuilder()
+        .registerTypeAdapter(LocalDateTime.class, new LocalDateTimeAdapter())
+        .create();
 
     public void sendConnectRoom(Integer empId, String sessionId, String roomId) {
         log.info("연결 요청: 세션 ID {}, 사원 ID {}, 방 ID {}", sessionId, empId, roomId);
         try {
             ConnectRoomMessage message = new ConnectRoomMessage(empId, sessionId, roomId);
-            String kafkaMessage = new Gson().toJson(message);
+            String kafkaMessage = gson.toJson(message);
             kafkaTemplate.send("connect_room_topic", kafkaMessage);
             log.info("연결 요청이 Kafka에 전송되었습니다.");
         } catch (Exception e) {
@@ -40,7 +47,7 @@ public class StompToKafkaService {
         log.info("주문 아이템 추가 요청: 세션 ID {}, 요청 DTO {}", sessionId, requestDto);
         try {
             AddOrderItemMessage message = new AddOrderItemMessage(sessionId, requestDto);
-            String kafkaMessage = new Gson().toJson(message);
+            String kafkaMessage = gson.toJson(message);
             kafkaTemplate.send("add_order_item_topic", kafkaMessage);
             log.info("주문 아이템 추가 요청이 Kafka에 전송되었습니다.");
         } catch (Exception e) {
@@ -52,7 +59,7 @@ public class StompToKafkaService {
         log.info("주문 아이템 삭제 요청: 세션 ID {}, 주문 아이템 ID {}", sessionId, orderItemId);
         try {
             DeleteOrderItemMessage message = new DeleteOrderItemMessage(sessionId, orderItemId);
-            String kafkaMessage = new Gson().toJson(message);
+            String kafkaMessage = gson.toJson(message);
             kafkaTemplate.send("delete_order_item_topic", kafkaMessage);
             log.info("주문 아이템 삭제 요청이 Kafka에 전송되었습니다.");
         } catch (Exception e) {
@@ -64,7 +71,7 @@ public class StompToKafkaService {
         log.info("게임 시작 요청: 세션 ID {}", sessionId);
         try {
             StartGameMessage message = new StartGameMessage(sessionId);
-            String kafkaMessage = new Gson().toJson(message);
+            String kafkaMessage = gson.toJson(message);
             kafkaTemplate.send("start_game_topic", kafkaMessage);
             log.info("게임 시작 요청이 Kafka에 전송되었습니다.");
         } catch (Exception e) {
@@ -76,7 +83,7 @@ public class StompToKafkaService {
         log.info("원판 돌리기 요청: 세션 ID {}", sessionId);
         try {
             RunWheelMessage message = new RunWheelMessage(sessionId);
-            String kafkaMessage = new Gson().toJson(message);
+            String kafkaMessage = gson.toJson(message);
             kafkaTemplate.send("run_wheel_topic", kafkaMessage);
             log.info("원판 돌리기 요청이 Kafka에 전송되었습니다.");
         } catch (Exception e) {
@@ -88,7 +95,7 @@ public class StompToKafkaService {
         log.info("주문 요청: 세션 ID {}, 요청 DTO {}", sessionId, orderRequestDto);
         try {
             OrderMessage message = new OrderMessage(sessionId, orderRequestDto);
-            String kafkaMessage = new Gson().toJson(message);
+            String kafkaMessage = gson.toJson(message);
             kafkaTemplate.send("order_topic", kafkaMessage);
             log.info("주문 요청이 Kafka에 전송되었습니다.");
         } catch (Exception e) {
@@ -100,7 +107,7 @@ public class StompToKafkaService {
         log.info("방 떠나기 요청: 세션 ID {}", sessionId);
         try {
             LeaveRoomMessage message = new LeaveRoomMessage(sessionId);
-            String kafkaMessage = new Gson().toJson(message);
+            String kafkaMessage = gson.toJson(message);
             kafkaTemplate.send("leave_room_topic", kafkaMessage);
             log.info("방 떠나기 요청이 Kafka에 전송되었습니다.");
         } catch (Exception e) {
