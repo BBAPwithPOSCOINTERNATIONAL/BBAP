6db43d233e689fae197dec9afbd211998ec4bb61
김다희 <kdahui98@gmail.com>
2024-05-18 18:58:07 +0900

    Feat : kafkalistner에도 localdatetimeadaptor 적용

--- backend/order_room/src/main/java/com/bbap/order_room/config/OrderItemConsumer.java
+++ backend/order_room/src/main/java/com/bbap/order_room/config/OrderItemConsumer.java
@@ -1,9 +1,12 @@
 package com.bbap.order_room.config;
 
+import java.time.LocalDateTime;
+
 import com.bbap.order_room.dto.kafkamessage.*;
 import com.bbap.order_room.service.WebSocketService;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -16,10 +19,13 @@ import org.springframework.stereotype.Service;
 public class OrderItemConsumer {
 
     private final WebSocketService webSocketService;
+    private final Gson gson = new GsonBuilder()
+        .registerTypeAdapter(LocalDateTime.class, new LocalDateTimeAdapter())
+        .create();
     @KafkaListener(topics = "connect_room_topic", groupId = "order_room_group")
     public void consumeConnectRoom(String message) {
         try {
-            ConnectRoomMessage connectRoomMessage = new Gson().fromJson(message, ConnectRoomMessage.class);
+            ConnectRoomMessage connectRoomMessage = gson.fromJson(message, ConnectRoomMessage.class);
             log.info("Kafka에서 수신한 연결 요청 메시지: {}", connectRoomMessage);
             webSocketService.connectRoom(connectRoomMessage.getEmpId(), connectRoomMessage.getSessionId(), connectRoomMessage.getRoomId());
         } catch (Exception e) {
@@ -30,7 +36,7 @@ public class OrderItemConsumer {
     @KafkaListener(topics = "add_order_item_topic", groupId = "order_room_group")
     public void consumeAddOrderItem(String message) {
         try {
-            AddOrderItemMessage addOrderItemMessage = new Gson().fromJson(message, AddOrderItemMessage.class);
+            AddOrderItemMessage addOrderItemMessage = gson.fromJson(message, AddOrderItemMessage.class);
             log.info("Kafka에서 수신한 주문 아이템 추가 메시지: {}", addOrderItemMessage);
             webSocketService.addOrderItem(addOrderItemMessage.getSessionId(), addOrderItemMessage.getRequestDto());
         } catch (Exception e) {
@@ -41,7 +47,7 @@ public class OrderItemConsumer {
     @KafkaListener(topics = "delete_order_item_topic", groupId = "order_room_group")
     public void consumeDeleteOrderItem(String message) {
         try {
-            DeleteOrderItemMessage deleteOrderItemMessage = new Gson().fromJson(message, DeleteOrderItemMessage.class);
+            DeleteOrderItemMessage deleteOrderItemMessage = gson.fromJson(message, DeleteOrderItemMessage.class);
             log.info("Kafka에서 수신한 주문 아이템 삭제 메시지: {}", deleteOrderItemMessage);
             webSocketService.deleteOrderItem(deleteOrderItemMessage.getSessionId(), deleteOrderItemMessage.getOrderItemId());
         } catch (Exception e) {
@@ -52,7 +58,7 @@ public class OrderItemConsumer {
     @KafkaListener(topics = "start_game_topic", groupId = "order_room_group")
     public void consumeStartGame(String message) {
         try {
-            StartGameMessage startGameMessage = new Gson().fromJson(message, StartGameMessage.class);
+            StartGameMessage startGameMessage = gson.fromJson(message, StartGameMessage.class);
             log.info("Kafka에서 수신한 게임 시작 메시지: {}", startGameMessage);
             webSocketService.startGame(startGameMessage.getSessionId());
         } catch (Exception e) {
@@ -63,7 +69,7 @@ public class OrderItemConsumer {
     @KafkaListener(topics = "run_wheel_topic", groupId = "order_room_group")
     public void consumeRunWheel(String message) {
         try {
-            RunWheelMessage runWheelMessage = new Gson().fromJson(message, RunWheelMessage.class);
+            RunWheelMessage runWheelMessage = gson.fromJson(message, RunWheelMessage.class);
             log.info("Kafka에서 수신한 원판 돌리기 메시지: {}", runWheelMessage);
             webSocketService.runWheel(runWheelMessage.getSessionId());
         } catch (Exception e) {
@@ -74,7 +80,7 @@ public class OrderItemConsumer {
     @KafkaListener(topics = "order_topic", groupId = "order_room_group")
     public void consumeOrder(String message) {
         try {
-            OrderMessage orderMessage = new Gson().fromJson(message, OrderMessage.class);
+            OrderMessage orderMessage = gson.fromJson(message, OrderMessage.class);
             log.info("Kafka에서 수신한 주문 요청 메시지: {}", orderMessage);
             webSocketService.order(orderMessage.getSessionId(), orderMessage.getOrderRequestDto());
         } catch (Exception e) {
@@ -85,7 +91,7 @@ public class OrderItemConsumer {
     @KafkaListener(topics = "leave_room_topic", groupId = "order_room_group")
     public void consumeLeaveRoom(String message) {
         try {
-            LeaveRoomMessage leaveRoomMessage = new Gson().fromJson(message, LeaveRoomMessage.class);
+            LeaveRoomMessage leaveRoomMessage = gson.fromJson(message, LeaveRoomMessage.class);
             log.info("Kafka에서 수신한 방 떠나기 메시지: {}", leaveRoomMessage);
             webSocketService.leaveRoom(leaveRoomMessage.getSessionId());
         } catch (Exception e) {
