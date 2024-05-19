70abc80e1f28d444d1fbb57ed2e4c44e9b16d936
이성완 <cjddktkdwk@naver.com>
2024-05-20 02:26:32 +0900

    Revert "Feat : async"
    This reverts commit 6086e9ca174e0a0f64401c6988500b494c2ba85c.

--- backend/payment/src/main/java/com/bbap/payment/PaymentApplication.java
+++ backend/payment/src/main/java/com/bbap/payment/PaymentApplication.java
@@ -3,11 +3,9 @@ package com.bbap.payment;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
-import org.springframework.scheduling.annotation.EnableAsync;
 
 @SpringBootApplication
 @EnableFeignClients
-@EnableAsync
 public class PaymentApplication {
 
 	public static void main(String[] args) {
